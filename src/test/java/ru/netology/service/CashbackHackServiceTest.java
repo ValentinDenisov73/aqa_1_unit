6ec4f31d2b3925b64ba78bc60378ec75761ea4d4
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-07 17:23:57 +0400

    add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemainIfLessThan1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void shouldRemainIfMoreThan1000() {
+        int amount = 1100;
+        int expected = 900;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void shouldNotRemainIfEquals1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+}
