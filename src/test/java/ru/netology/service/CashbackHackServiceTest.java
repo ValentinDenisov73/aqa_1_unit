9f0033ce40381627a3968037f5d3287d553a5994
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-07 16:56:13 +0400

    add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemainIfLessThan1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void shouldRemainIfMoreThan1000() {
+        int amount = 1100;
+        int expected = 900;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void shouldNotRemainIf1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual,expected);
+    }
+}
