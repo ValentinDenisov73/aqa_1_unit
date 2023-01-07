5fcf39adb91822e21074a9fec58faf9aa5fdcaec
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-07 21:36:25 +0400

    add test jupiter

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJupiter.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTestJupiter {
+
+    @Test
+    public void shouldRemainIfLessThan1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemainIfMoreThan1000() {
+        int amount = 1100;
+        int expected = 900;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotRemainIfEquals1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+    }
+}
