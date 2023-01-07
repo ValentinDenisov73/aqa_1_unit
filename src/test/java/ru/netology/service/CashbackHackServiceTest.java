4b5c80913d24c855da819d7c99af46bb9f6eb943
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-07 19:36:26 +0400

    add dependencies junit vintage

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.service;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
 
     @Test
@@ -11,22 +12,24 @@ public class CashbackHackServiceTest {
         int expected = 100;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void shouldRemainIfMoreThan1000() {
         int amount = 1100;
         int expected = 900;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void shouldNotRemainIfEquals1000() {
         int amount = 1000;
         int expected = 0;
         CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
